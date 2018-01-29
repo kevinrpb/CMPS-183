3964b118b7e04cef17449207f6ef8be8cfe6596a
Ron Marks <romarks@ucsc.edu>
2018-01-28 16:04:29 -0800

    Initial firebase integration

--- views/PageContent.jsx
+++ views/PageContent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
 import LandingPage from './components/Landing_Page/Landing.jsx';
 
 class PageContent extends React.Component {
@@ -8,6 +9,16 @@ class PageContent extends React.Component {
             <LandingPage />
         );
     }
+}
+
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyC_VcsH5WT0RryJaje9-1nvy2M1IsoGt2I",
+        authDomain: "textbookheroes-6b61a.firebaseapp.com",
+        databaseURL: "https://textbookheroes-6b61a.firebaseio.com",
+        projectId: "textbookheroes-6b61a",
+        storageBucket: "textbookheroes-6b61a.appspot.com",
+    });
 }
 
 if (typeof window !== 'undefined') {
