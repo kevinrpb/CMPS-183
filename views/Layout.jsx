ac65cf6fb08c77fbc0bff8c5d742fdb0070a075d
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2018-03-03 14:37:13 -0800

    Fixed static files refs

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -9,7 +9,7 @@ class Layout extends React.Component {
 				<title>{this.props.title}</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
-				<link rel="stylesheet" href="styles.css" />
+				<link rel="stylesheet" href="/styles.css" />
 			</head>
 			<body> 
 				<div id="root">{this.props.children}</div>
@@ -17,7 +17,7 @@ class Layout extends React.Component {
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
 				<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-				<script type="text/javascript" src="./bundle.js"></script>
+				<script type="text/javascript" src="/bundle.js"></script>
 			</body>
 			</html>
 			);
