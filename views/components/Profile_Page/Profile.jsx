3e797c20a9ad29523e87e40122bc37fac1aedee8
Katerina Chinnappan <agukalpa@Katerinas-MacBook-Pro.local>
2018-03-07 14:24:24 -0800

    stuff

--- views/components/Profile_Page/Profile.jsx
+++ views/components/Profile_Page/Profile.jsx
@@ -7,7 +7,7 @@ import './Profile.css';
 const User = (props) => {
 	return (
 		<center>
-				<img src={props.items || "https://firebasestorage.googleapis.com/v0/b/textbookheroes-2df75.appspot.com/o/822711_user_512x512.png?alt=media&token=5d733b17-00a9-4852-9ef3-87c2f3318fee"} alt="" width="150" height="150"/>
+				<img src={"https://firebasestorage.googleapis.com/v0/b/textbookheroes-2df75.appspot.com/o/822711_user_512x512.png?alt=media&token=5d733b17-00a9-4852-9ef3-87c2f3318fee"} alt="" width="150" height="150"/>
 			</center>
 	);
 }
@@ -16,9 +16,8 @@ class Profile extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			items: props.data,
-			type: props.data,
-			query: props.data
+			name: props.data,
+			email: props.data,
 		}
 	}
 
